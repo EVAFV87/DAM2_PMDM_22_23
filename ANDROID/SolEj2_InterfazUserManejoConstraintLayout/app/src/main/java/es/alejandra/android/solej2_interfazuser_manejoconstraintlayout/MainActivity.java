b6cc19a9c121e5ff0ce5e1cb6b29fe0e407aa614
Mª Alejandra Tomás Fernández <alejandrtf@gmail.com>
2022-10-13 17:48:32 +0200

    Solución Ej2_Interfaz Usuario_Manejo-ConstraintLayout subido
    Solución del ejercicio de manejo de varios layouts hechos con ConstraintLayout: colores, calculadora,... subida ya

---
+++ ANDROID/SolEj2_InterfazUserManejoConstraintLayout/app/src/main/java/es/alejandra/android/solej2_interfazuser_manejoconstraintlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package es.alejandra.android.solej2_interfazuser_manejoconstraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_complejo);
+    }
+}
